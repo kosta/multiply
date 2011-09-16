a3d6a712f4c8b86948fa34ae2c240e86f40806ba
Philipp Henkel <henkel42@gmail.com>
2011-09-16 15:58:48 +0200

    added multiply.py

---
+++ multiply.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+#
+# Multiply - Toolbox for Mathematics
+# Author: Philipp
+#
+
+def multiply_by_2(numbers):
+   result = []
+   for number in numbers:
+      result.append(number * 2)
+   return result
+
+
+def main():
+   input = [1, 2, 3, 4]
+   output = multiply_by_2(input)
+   print(str(input) +' -> '+ str(output))
+
+
+if __name__ == '__main__':
+    main()
+
