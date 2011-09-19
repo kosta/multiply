2a48cfecac5d291a87690ff7c10b291dd6264545
Kosta Welke <Kosta Welke>
2011-09-19 13:51:39 +0200

    actually use command line arguments

---
+++ asyncwaiter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# 
+# AsyncWaiter - Toolbox doing stuff with AsyncWaiter
+# Author: Kosta
+# 
+
+def executeNow(f, *args):
+    return f(*args)
+
--- multiply.py
+++ multiply.py
@@ -7,18 +7,26 @@
 # Author: Philipp
 #
 
+import sys
+import asyncwaiter
+
 def multiply_by_2(numbers):
     result = []
     for number in numbers:
         result.append(number * 2)
     return result
 
+def is_integer(x):
+    try:
+        int(x)
+        return True
+    except ValueError:
+        return False
 
 def main():
-    input = [1, 2, 3, 4]
-    output = multiply_by_2(input)
+    input = [int(x) for x in sys.argv[1:] if is_integer(x)]
+    output = asyncwaiter.executeNow(multiply_by_2, input)
     print(str(input) +' -> '+ str(output))
-
 
 if __name__ == '__main__':
      main()
